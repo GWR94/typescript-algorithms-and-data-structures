6dbc144e0b296eb18ae5d7329710c2900ccb0bb0
James <james_gower@outlook.com>
2019-05-16 09:08:59 +0100

    completed more advanced recursion challenges

---
+++ algorithms/recursion/advanced-challenges/is-palindrome.ts
@@ -0,0 +1,13 @@
+function isPalindrome(str: string) {
+	if (str[0] !== str[str.length - 1]) {
+		console.log("Not a palindrome");
+		return false;
+	}
+	if (str.length <= 1) {
+		console.log("Palindrome");
+		return true;
+	}
+	return isPalindrome(str.slice(1, -1));
+}
+
+isPalindrome("sabcddcbas");
---
+++ algorithms/recursion/advanced-challenges/some-recursive.ts
@@ -0,0 +1,12 @@
+function someRecursive(arr: any[], func: Function) {
+	if (arr.length === 0) {
+		return false;
+	}
+	if (func(arr[0])) return true;
+	return someRecursive(arr.slice(1), func);
+}
+
+const isOdd = num => {
+	return num % 2 === 0;
+};
+someRecursive([2, 4, 6], isOdd);
