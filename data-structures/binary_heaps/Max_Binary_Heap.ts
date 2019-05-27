4d44cecc6da83c3288eebf2125d789c6ab218c9e
James <james_gower@outlook.com>
2019-05-27 10:06:11 +0100

    completed and commented MaxBinaryHeap class

--- data-structures/binary_heaps/Max_Binary_Heap.ts
+++ data-structures/binary_heaps/Max_Binary_Heap.ts
@@ -1,31 +1,200 @@
-// import swap from "../../algorithms/sorting-algorithms/utils/swap";
-/**
- * TODO
- * [ ] Fix insert method
- */
 export default class MaxBinaryHeap {
-	public values: any[];
+	private values: any[];
 	constructor(data?: any[]) {
-		this.values = [39, 41, 18, 27, 12, 33];
+		this.values = [];
+		/**
+		 * If any values are passed in as a parameter,
+		 * execute the insert method on each of the
+		 * elements
+		 */
+		if (data) data.forEach(element => this.insert(element));
 	}
 
-	public insert(value: any) {
-		const swap = (arr: any[], idx1: number, idx2: number): void => {
-			[arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-		};
+	public getValues = (): void => {
+		return console.log(this.values);
+	};
+
+	/**
+	 * Method to insert a value into the max binary heap.
+	 * @param value - the value to insert into the binary
+	 * tree
+	 */
+	public insert = (value: number | string): void => {
+		/**
+		 * Push the value to the end of the array, and then
+		 * use the bubble up method to move the value up
+		 * the binary heap into its correct spot.
+		 */
 		this.values.push(value);
-		let child: any = this.values.length - 1;
-		let parent: any = Math.floor(child / 2);
-		while (this.values[parent] < value) {
-			console.log(this.values);
-			swap(this.values, parent, child);
-			child = parent;
-			parent = Math.floor(child / 2);
+		this.bubbleUp();
+	};
+
+	/**
+	 * Method to remove the maximum value from the binary
+	 * heap, and rearrange all of the other elements so it
+	 * is a valid heap once more.
+	 */
+	public extractMax = (): number | string => {
+		/**
+		 * Swap the end value with the current max value
+		 * (root), and store them in constants.
+		 */
+		const max: number | string = this.values[0];
+		const end: number | string = this.values.pop();
+		if (this.values.length > 0) {
+			this.values[0] = end;
+			/**
+			 * Execute the sinkDown method to move the previous
+			 * end (now first) element to it's correct spot.
+			 */
+			this.sinkDown();
+			/**
+			 * Return the max elements as this is what was
+			 * removed from the heap.
+			 */
 		}
-	}
+		return max;
+	};
+
+	/**
+	 * Method to swap the two values in the values array
+	 * @param idx1 - the index of the first value to swap
+	 * @param idx2 - the index of the second value to swap
+	 */
+	protected swap = (idx1: number, idx2: number): void => {
+		const arr = this.values;
+		[arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+	};
+
+	/**
+	 * Method to sort the binary heap once the an element has
+	 * been added to the end, as it's more than likely not in
+	 * its correct spot.
+	 */
+	protected bubbleUp = (): void => {
+		/**
+		 * Create a let to store the index which the element that
+		 * was pushed into the array (the last element in the
+		 * values array), and create a constant for the value of
+		 * that index so it can be compared later.
+		 */
+		let idx: number = this.values.length - 1;
+		const element: number | string = this.values[idx];
+		/**
+		 * Loop through the binary heap while the index is valid
+		 * (above 0).
+		 */
+		while (idx > 0) {
+			/**
+			 * Find the parent index of the current idx ((n-1)/2),
+			 * store it in a constant and save the value of that
+			 * index in the parent constant.
+			 */
+			const parentIdx: number = Math.floor((idx - 1) / 2);
+			const parent: number | string = this.values[parentIdx];
+			/**
+			 * If the parent is equal to or larger than the element,
+			 * then break the loop as it's in its correct spot.
+			 */
+			if (element <= parent) break;
+			/**
+			 * If the element is smaller, then you need to swap the
+			 * values (using the swap method), and set idx to be
+			 * parentIdx because that is where the element will now
+			 * be in the binary heap. Keep looping until the value
+			 * is in its correct spot.
+			 */
+			this.swap(parentIdx, idx);
+			idx = parentIdx;
+		}
+	};
+
+	/**
+	 * Method to move the element down the binary heap after extractMax
+	 * has moved the last element to the root, because that element will
+	 * be incorrectly placed for a valid binary heap.
+	 */
+	protected sinkDown = (): void => {
+		/**
+		 * Create a variable to store the current index, and set it to 0,
+		 * then set the element constant to be the value from idx
+		 * because the first element in the values array is the current
+		 * variable that needs to be compared to others.
+		 */
+		let idx = 0;
+		const element = this.values[0];
+		/**
+		 * Store the length of the values array so it can be used to check
+		 * for a valid index in the while loop.
+		 */
+		const length = this.values.length;
+		while (true) {
+			/**
+			 * Save the index of the left and right child of the current index
+			 * in constants, and create two lets to store the value of these
+			 * children later (if they are valid indexes).
+			 */
+			const leftIdx: number = 2 * idx + 1;
+			const rightIdx: number = 2 * idx + 2;
+			let leftChild: number | string;
+			let rightChild: number | string;
+			/**
+			 * Create a swap variable and initialise it to be null, so it can
+			 * later be set to the left or right child to swap at the end of the
+			 * loop if necessary.
+			 */
+			let swap: number = null;
+			/**
+			 * If the index of the left child is less than the length, then it is
+			 * a valid value, so leftChild can be set to be the left child's index.
+			 * If leftChild is larger than the current element, then it needs to
+			 * be swapped, so set the swap variable to be leftIdx.
+			 */
+			if (leftIdx < length) {
+				leftChild = this.values[leftIdx];
+				if (leftChild > element) swap = leftIdx;
+			}
+			/**
+			 * If index of the right child is less than the length, then it is a
+			 * valid value, so rightChild can eb set to the right child's index.
+			 */
+			if (rightIdx < length) {
+				rightChild = this.values[rightIdx];
+				/**
+				 * If no swap has been made in the previous if statement (swap is
+				 * null) and the right child is larger than the element, then swap
+				 * needs to be set to the right child's index. If there was a swap
+				 * in the previous if statement (swap is not null) and the right
+				 * child is larger than the left child, swap should be set to be
+				 * the right child's index as you should only swap the larger element.
+				 */
+				if (
+					(swap === null && rightChild > element) ||
+					(swap !== null && rightChild > leftChild)
+				) {
+					swap = rightIdx;
+				}
+			}
+			/**
+			 * If there was no changes to the swap variable, then you need to break
+			 * out of the loop because the element is in the right place.
+			 */
+			if (swap === null) break;
+			/**
+			 * If the previous if statement hasn't broken out of the loop, then a swap
+			 * needs to occur - using the swap method. The idx variable needs to be set
+			 * to be swap so the loop can be continued from that index.
+			 */
+			this.swap(idx, swap);
+			idx = swap;
+		}
+	};
 }
 
-const heap = new MaxBinaryHeap();
+const heap = new MaxBinaryHeap([55, 39, 27, 12, 33, 41, 18]);
+heap.getValues();
+heap.extractMax();
+heap.getValues();
 
-heap.insert(55);
-console.log(heap.values);
+// [55, 39, 41, 18, 27, 12, 33]
+// [41, 39, 33, 18, 27, 12]
